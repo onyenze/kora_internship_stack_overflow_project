de41c6f080732c21b3df7c7f18f9bafba94cd14a
onyenze <chibuezeonyenze123@gmail.com>
2023-12-06 23:21:14 +0100

    working hard

--- jest.config.js
+++ jest.config.js
@@ -2,15 +2,8 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFiles: ['<rootDir>/src/__tests__/acceptance/users.tests.ts'],
-    testMatch: ['<rootDir>/src/__tests__/acceptance/users.tests.ts'],
-    globals: {
-      'ts-jest': {
-        tsconfig: 'tsconfig.json',
-      },
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
     },
   };
   
--- src/__tests__/acceptance/users.tests.ts
+++ src/__tests__/acceptance/users.tests.ts
@@ -1,14 +1,29 @@
 // import { port } from "../../config/"
-import { createConnection } from "mysql2/promise"
+import request from "supertest";
 import app from "../../app"
 import sequelize from "../../config/dbconfig";
-
-let connection, server;
+import db from "../../models"
+let connection:any, server:any;
 
 beforeEach(async()=>{
-    await sequelize.sync(); 
-    server = app.listen(7000)
+    connection  = await db.sequelize(); 
+    await connection.sync()
+    server = app.listen(8000)
 })
+
+
+
+afterAll( () => {
+     connection.close();
+     server.close()
+  });
+
+
+  
+it("should get all users", async()=>{
+    const response = await request(app).get("/users")
+})
+
 
 
 
