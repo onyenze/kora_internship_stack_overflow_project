d156d056c8ef86f96d1de95b214798988b39ee9e
onyenze <chibuezeonyenze123@gmail.com>
2024-01-09 20:50:28 +0100

    commenting

--- src/__tests__/acceptance/users.tests.ts
+++ src/__tests__/acceptance/users.tests.ts
@@ -1,34 +1,34 @@
-// import { port } from "../../config/"
-import request from "supertest";
-import app from "../../app"
-import sequelize from "../../config/dbconfig";
-import db from "../../models"
-let connection:any, server:any;
+// // import { port } from "../../config/"
+// import request from "supertest";
+// import app from "../../app"
+// import sequelize from "../../config/dbconfig";
+// import db from "../../models"
+// let connection:any, server:any;
 
 
 
-beforeEach(async()=>{
-    connection  = await db.sequelize(); 
-    await connection.sync()
-    server = app.listen(8000)
-})
+// beforeEach(async()=>{
+//     connection  = await db.sequelize(); 
+//     await connection.sync()
+//     server = app.listen(8000)
+// })
 
 
 
 
 
-afterAll( () => {
-     connection.close();
-     server.close()
-  });
+// afterAll( () => {
+//      connection.close();
+//      server.close()
+//   });
+
 
 
 
   
-  
-it("should get all users", async()=>{
-    const response = await request(app).get("/users")
-})
+// it("should get all users", async()=>{
+//     const response = await request(app).get("/users")
+// })
 
 
 
